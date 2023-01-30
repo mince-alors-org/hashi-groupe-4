6147c60f74dcbed1d14af20134c71e051845924a
GTerLeon <Leon.Ter.Etu@univ-lemans.fr>
2023-01-30 16:19:21 +0100

    fix de actualiserFichier() avec Pont

--- src/SauvegardeGrille.java
+++ src/SauvegardeGrille.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class SauvegardeGrille{  
 
     private FileWriter fileWriter;  //écrire dans un fichier
-    ArrayList<Pont> pileCoups = new ArrayList<Pont>();  //pile des coups joués et actifs sur la grille
-    ArrayList<Pont> pileRetablissements = new ArrayList<Pont>();    //pile des coups précédemment joués mais annulés
-    int taillePileCoups = pileCoups.size();
+    private ArrayList<Pont> pileCoups = new ArrayList<Pont>();  //pile des coups joués et actifs sur la grille
+    private ArrayList<Pont> pileRetablissements = new ArrayList<Pont>();    //pile des coups précédemment joués mais annulés
+    private int taillePileCoups = pileCoups.size();
     
     public SauvegardeGrille(){
         try{
@@ -22,8 +22,6 @@ public class SauvegardeGrille{
     public void retablir(){
         pileCoups.add(pileRetablissements.get(taillePileCoups-1));
         pileRetablissements.remove(taillePileCoups-1);
-        pileCoups.add(pileRetablissements.get(taillePileCoups-2));
-        pileRetablissements.remove(taillePileCoups-2);
     }
 
     /**
@@ -32,13 +30,11 @@ public class SauvegardeGrille{
     public void annuler(){ 
         pileRetablissements.add(pileCoups.get(taillePileCoups-1));
         pileCoups.remove(taillePileCoups-1);
-        pileRetablissements.add(pileCoups.get(taillePileCoups-2));
-        pileCoups.remove(taillePileCoups-2);
     }
 
     /**
      * ajout du coup dans la pileCoups
-     * @param p1 
+     * @param p1 pont joué et actif sur la grille
      */
     public void ajoutCoup(Pont p1){ 
         pileCoups.add(p1);
@@ -48,12 +44,18 @@ public class SauvegardeGrille{
      * sauvegarde des coups dans le fichier
      */
     public void actualiserFichier(){
-        Pont p1 = pileCoups.get(taillePileCoups-1);
-        try{
-            fileWriter.write(p1.coords[1] + p1.coords[2] + "\n");
-            fileWriter.flush();
-        } catch (IOException e){
-            e.printStackTrace();
+        
+        for (int i = 0; i < taillePileCoups; i++){
+            Pont p1 = pileCoups.get(i);
+            try{
+                fileWriter.write(p1.getCoords1()+ p1.getCoords2() + "\n");
+                fileWriter.flush();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
         }
+
+       
     }
+
 }
