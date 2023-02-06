7d523caf35649e04c1f87588c27110855faff2d2
Pechon Morgane <i190240@ic2s120-15.univ-lemans.fr>
2023-02-06 16:09:32 +0100

    pouet

--- hashi/src/test/java/com/monappli/test_Ile_pont.java
+++ hashi/src/test/java/com/monappli/test_Ile_pont.java
@@ -5,6 +5,6 @@ public class test_Ile_pont{
 	@Test
 	public void creaPont(){
 		Pont p= new Pont(new Ilot(10,5,3),new Ilot(9,6,4));
-		assertTrue(1,414213562 == p.tailleTrait());
+		assertTrue(Math.sqrt(2) == p.tailleTrait());
 	}
 }
