5472bf3448c0a08ad19f60dd6dd10613ea41612f
Nail Leo <s193670@ic2s120-14.univ-lemans.fr>
2023-02-06 11:23:59 +0100

    mise à jour de Pont.java (commentaire)

--- hashi/src/main/java/com/monappli/Pont.java
+++ hashi/src/main/java/com/monappli/Pont.java
@@ -1,9 +1,18 @@
 import java.util.ArrayList;
 import java.math.*;
 
+/**
+ * 
+ * @author
+ */
 public class Pont {
 	int nombreTraits;
 	ArrayList <Ilot> coords;
+	/**
+	 * @param ile1 :
+	 * @param ile2 :
+	 * @author 
+	 */
 	Pont(Ilot ile1, Ilot ile2){
 		this.nombreTraits=0;
 		coords =new ArrayList <>();
@@ -12,6 +21,10 @@ public class Pont {
 		ile1.addPont(this);
 		ile2.addPont(this);
 	}
+
+	/**
+	 * @author
+	 */
 	public void incrementer(){
 		if(this.nombreTraits == 2)
 			this.nombreTraits=0;
@@ -19,15 +32,12 @@ public class Pont {
 			this.nombreTraits++;
 	}
 
+	/**
+	 * @return
+	 * @author
+	 */
 	public int tailleTrait(){
 		return (int) Math.sqrt(Math.pow((coords.get(1).getPosX()-coords.get(2).getPosX()),2)+Math.pow((coords.get(1).getPosY()-coords.get(2).getPosY()),2));
-	}
-	public static void main(String[] args){
-		Ilot ilot1 = new Ilot(2,5,3);
-		Ilot ilot2 = new Ilot(4,3,5);
-		Pont pont =new Pont(ilot1,ilot2);
-		System.out.println(pont.tailleTrait());
-		System.out.println(ilot1.valide());
 	}
 
 }
