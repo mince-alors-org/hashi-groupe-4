20f1c8427a1e21452fab9d437211dd54c25696ef
Pechon Morgane <i190240@ic2s120-15.univ-lemans.fr>
2023-02-06 16:18:50 +0100

    mise à jour de la branche grille

--- hashi/src/test/java/com/monappli/test_Ile_pont.java
+++ hashi/src/test/java/com/monappli/test_Ile_pont.java
@@ -11,10 +11,7 @@ public class test_Ile_pont{
 	@Test
 	public void creaPont(){
 		Pont p= new Pont(new Ilot(10,5,3),new Ilot(9,6,4));
-<<<<<<< HEAD
 		assertTrue(Math.sqrt(2) == p.tailleTrait());
-=======
-    assertEquals(Math.sqrt(2), p.tailleTrait());
->>>>>>> bf08f116a16ad550b67e0bfac3ce20ed7f1fdad4
+		assertEquals(Math.sqrt(2), p.tailleTrait());
 	}
 }
