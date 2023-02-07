3d90477b9c35dbc0ded57be1dda3cc2c49129a60
ForLunt <pechon.morgane@gmail.com>
2023-02-07 21:40:18 +0100

    mise à jour de pont

--- hashi/src/main/java/com/monappli/Ilot.java
+++ hashi/src/main/java/com/monappli/Ilot.java
@@ -2,124 +2,170 @@ package main.java.com.monappli;
 
 import java.util.ArrayList;
 
+import java.util.Collections;
+
 /**
  * Cette classe permet de représenter un Ilot
  * @author nmention
  */
-public class Ilot {
-    /**
-     * position de this dans l'axe des abscisses (X)
-     */
-    private int posX;
-    /**
-     * position de this dans l'axe des ordonnées (Y)
-     */
-    private int posY;
-    /**
-     * nombre de ponts supportés par this
-     */
-    private int valeur;
+public class Ilot implements Comparable<Ilot>{
+	/**
+	 * position de this dans l'axe des abscisses (X)
+	 */
+	private int posX;
+	/**
+	 * position de this dans l'axe des ordonnées (Y)
+	 */
+	private int posY;
+	/**
+	 * nombre de ponts supportés par this
+	 */
+	private int valeur;
 
-    /**
-     * ponts reliés à this
-     */
-    private ArrayList<Pont> ponts;
+	/**
+	 * ponts reliés à this
+	 */
+	private ArrayList<Pont> ponts;
 
-    private ArrayList<Pont> pontsSolution;
+	private ArrayList<Pont> pontsSolution;
 
-    /**
-     *
-     * @param posX position du l'ilot courant dans l'axe des abscisses (X)
-     * @param posY position de l'ilot courant dans l'axe des ordonnées (Y)
-     * @param valeur nombre de ponts supportés par this
+	/**
+	 *
+	 * @param posX position du l'ilot courant dans l'axe des abscisses (X)
+	 * @param posY position de l'ilot courant dans l'axe des ordonnées (Y)
+	 * @param valeur nombre de ponts supportés par this
+	*/
+	public Ilot(int posX, int posY, int valeur) {
+		this.posX = posX;
+		this.posY = posY;
+		this.valeur = valeur;
+		ponts = new ArrayList<>();
+		this.pontsSolution = new ArrayList<>() ;
+	}
+	public int getPosX() {
+		return posX;
+	}
 
-     */
-    public Ilot(int posX, int posY, int valeur) {
-        this.posX = posX;
-        this.posY = posY;
-        this.valeur = valeur;
-        ponts = new ArrayList<>();
-        this.pontsSolution = new ArrayList<>() ;
-    }
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
 
+	public int getPosY() {
+		return posY;
+	}
 
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
 
+	public int getValeur() {
+		return valeur;
+	}
 
-
-
-
-    public int getPosX() {
-        return posX;
-    }
-
-    public void setPosX(int posX) {
-        this.posX = posX;
-    }
-
-    public int getPosY() {
-        return posY;
-    }
-
-    public void setPosY(int posY) {
-        this.posY = posY;
-    }
-
-    public int getValeur() {
-        return valeur;
-    }
-
-    public void setValeur(int valeur) {
-        this.valeur = valeur;
-    }
+	public void setValeur(int valeur) {
+		this.valeur = valeur;
+	}
 
 
-    public ArrayList<Pont> getPonts() {
-        return ponts;
-    }
+	public ArrayList<Pont> getPonts() {
+		return ponts;
+	}
 
-    public void setPonts(ArrayList<Pont> ponts) {
-        this.ponts = ponts;
-    }
+	public void setPonts(ArrayList<Pont> ponts) {
+		this.ponts = ponts;
+	}
 
-    /**
-     * Ajout d'un pont connecté au noeud courant
-     * @param pont : le pont à ajouter
-     */
-    public void addPont(Pont pont){
-        ponts.add(pont);
-    }
-
-
-    public void addPontSolution(Pont pont){
-        pontsSolution.add(pont);
-    }
-
-    /**
-     * Suppression d'un pont connecté au noeud courant
-     * @param pont : le pont à supprimer
-     */
-    public void deletePont(Pont pont){
-        ponts.remove(pont);
-    }
+	/**
+	 * Ajout d'un pont connecté au noeud courant
+	 * @param pont : le pont à ajouter
+	 */
+	public void addPont(Pont pont){
+		ponts.add(pont);
+	}
 
 
-    /**
-     * Indique si l'ilot peut être connecté à un pont
-     * @return true si l'ilot peut faire parti d'un pont
-     */
-    public boolean valide(){
-        return ponts.size() < valeur;
-    }
+	public void addPontSolution(Pont pont){
+		pontsSolution.add(pont);
+	}
 
-    @Override
-    public String toString() {
-        return "Ilot{" +
-                "posX=" + posX +
-                ", posY=" + posY +
-                ", valeur=" + valeur +
-                ", listePontsSolution=" + ponts +
-                '}';
-    }
+	/**
+	 * Suppression d'un pont connecté au noeud courant
+	 * @param pont : le pont à supprimer
+	 */
+	public void deletePont(Pont pont){
+		ponts.remove(pont);
+	}
+
+
+	/**
+	 * Indique si l'ilot peut être connecté à un pont
+	 * @return true si l'ilot peut faire parti d'un pont
+	 */
+	public boolean valide(){
+		return ponts.size() < valeur;
+	}
+
+	/**
+	 * Indique le nombre de pont activer
+	 * @return Le nombre de pont activer
+	 */
+	public int nbPont(){
+		int somme =0;
+		for(Pont p :this.pont){
+			somme+=p.nombreTraits;
+		}
+		return somme;
+	}
+
+	/**
+	 * Indique si le nombre de pont est dépasser ou non
+	 * @return vrai si le nombre de pont est trop grand faux si non
+	 */
+	public boolean pontDepasse(){
+		return this.nbPont()>this.valeur? true: false;
+	}
+	/**
+	 * Indique si le nombre de pont est valide
+	 * @return vrai si le nombre de pont est égale à la valeur faux si non
+	 */
+	public boolean pontValide(){
+		return this.nbPont()==this.valeur? true: false;
+	}
+	/**
+	 * Indique les voisin de l'ilot
+	 * @return ArrayList<Ilot> : retourne la liste des voisin de l'ile
+	 * @author Morgane
+	 */
+	public ArrayList<Ilot> listeVoisin(){
+		ArrayList<Ilot> voisin =new ArrayList<>();
+		for(Pont p : this.pont){
+			voisin.add(p.voisin(this));
+		}
+		return voisin;
+	}
+
+	/**
+	 * Pour vérifier si deux ile sont identique
+	 * @param Ilot : ile à comparer
+	 * @return vrai si les ile sont identique
+	 * @author Morgane
+	 */
+	public int compareTo(Ilot o) {
+		if (this.posX==o.posX && this.posY==o.posY)
+			return 1;
+		return 0;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Ilot{" +
+			"posX=" + posX +
+			", posY=" + posY +
+			", valeur=" + valeur +
+			", listePontsSolution=" + ponts +
+			'}';
+	}
 
 
 }
--- hashi/src/main/java/com/monappli/Pont.java
+++ hashi/src/main/java/com/monappli/Pont.java
@@ -3,18 +3,18 @@ import java.util.ArrayList;
 //import java.math.*;
 
 /**
- *
- * @author
+ *cette Objet permet l'utilisation des pont qui lie de ile entre elle
+ * @author Morgane
  */
 public class Pont {
 	int nombreTraits;
 	ArrayList <Ilot> coords;
 	/**
-	 * @param ile1 :
-	 * @param ile2 :
-	 * @author
+	 * @param ile1 : la où commence le pont
+	 * @param ile2 : la où termine le pont
+	 * @author Morgane
 	 */
-  public Pont(Ilot ile1, Ilot ile2){
+	public Pont(Ilot ile1, Ilot ile2){
 		this.nombreTraits=0;
 		coords =new ArrayList <>();
 		coords.add(ile1);
@@ -24,7 +24,8 @@ public class Pont {
 	}
 
 	/**
-	 * @author
+	 * Permet d'incrémenté le nombre de pont entre deux ile
+	 * @author Morgane
 	 */
 	public void incrementer(){
 		if(this.nombreTraits == 2)
@@ -32,14 +33,28 @@ public class Pont {
 		else
 			this.nombreTraits++;
 	}
+	/**
+	 * Permet de conaitre le voisin de l'île entrée
+	 * @param Ilot : ile qui demande sont voisin
+	 * @return Ilot : ile qui ce situe de l'autre côter du pont
+	 */
+	public Ilot voisin(Ilot demande){
+		return demande == coords.get(0) ? coords.get(1) : coords.get(0);
+	}
 
 	/**
-	 * @return
+	 * @return la liste des coordonné d'un pont
 	 * @author
 	 */
 	public ArrayList<Ilot> getCoords() {
 		return coords;
 	}
+
+	/**
+	 *Permet de connaître la taille d'un pont
+	 *@return la taille du pont
+	 @author Morgane
+	 */
 
 	public double tailleTrait(){
 		return Math.sqrt(Math.pow((coords.get(0).getPosX()-coords.get(1).getPosX()),2)+Math.pow((coords.get(0).getPosY()-coords.get(1).getPosY()),2));
