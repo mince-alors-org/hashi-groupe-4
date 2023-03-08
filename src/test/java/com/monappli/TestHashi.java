d8509d7646b3eb5724b4a15d646d321645b6a144
ForLunt <pechon.morgane@gmail.com>
2023-03-08 16:02:58 +0100

    test de l'interface

--- src/test/java/com/monappli/TestHashi.java
+++ src/test/java/com/monappli/TestHashi.java
@@ -4,12 +4,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
+import javafx.application.Application;
+
 public class TestHashi {
     
 	@BeforeEach
 	public void init(TestInfo testInfo){
         System.out.println("Debut test");
 	}
+    @Test
+    public void LancementInterface(){
+        System.out.println("Test interface");
+        Application.launch(Hashi.class);
+    }
     @Test
     public void afficheRes(){
         new Grille("1-1.niv");
