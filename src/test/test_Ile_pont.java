48ba50ec430dcc786d98a9ec906b30236e4f4599
Matthis1Collard <matthicollard@gmail.com>
2023-03-08 15:56:39 +0100

    petits changments

--- src/test/test_Ile_pont.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class test_Ile_pont{
-	@Test
-	public void creaPont(){
-		Pont p= new Pont(new Ilot(10,5,3),new Ilot(9,6,4));
-	}
-}
