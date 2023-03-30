12c402cacdef8dfbe17190c08492afd422ba12fa
Matthis1Collard <matthicollard@gmail.com>
2023-03-30 17:22:40 +0200

    Commentaires + changement param et quit

--- src/main/java/com/monappli/handlers/GameHandler.java
+++ src/main/java/com/monappli/handlers/GameHandler.java
@@ -1,47 +1,51 @@
 package com.monappli.handlers;
 
-import com.monappli.hashiScene.PopUp;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
+/**
+ * In game handler 
+ * @author Matthis Collard
+ */
 public class GameHandler extends DynamicEventHandler {
-    @FXML
-    private Pane gameBG;
 
+    /**
+     * Current game background pane
+     */
+    @FXML
+    private Pane backGround;
+
+    /**
+     * Parameters button
+     */
     @FXML
     private Button paramButton; 
 
+    /**
+     * Power Button. To quit
+     */
     @FXML
     private Button powerButton;
 
+    /**
+     * Reset button. To reset the whole grid
+     */
     @FXML
     private Button restButton;
 
+    /**
+     * Help Button
+     */
     @FXML
     private Button helpButton;
 
-
+    /**
+     * Initialization of GameHandler
+     * @param parent parent pane of the currently handled pane
+     */
     public GameHandler(Pane parent){
         super(parent);
-    }
-
-    public void quitClicked(){
-        Stage stage = (Stage) powerButton.getScene().getWindow();
-        stage.close();
-        System.out.println("Je quitte");
-    }
-
-    public void paramClicked() throws Exception{
-        if (gameBG.lookup("#pop") == null){
-            PopUp pop = new PopUp(this.getCurPane());
-            ParamHandler paramH= new GameParamHandler(this.gameBG);
-            pop.pasteAndHandle("/view/parameters.fxml", paramH);
-            paramH.setAll();
-            System.out.println("Je suis Paramètre");
-        }
     }
 
     public void restClicked(){
--- src/main/java/com/monappli/handlers/LevelSelectHandler.java
+++ src/main/java/com/monappli/handlers/LevelSelectHandler.java
@@ -1,53 +1,57 @@
 package com.monappli.handlers;
 
 import com.monappli.hashiScene.LevelScene;
-import com.monappli.hashiScene.PopUp;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
+/**
+ * Handler of the level selecting pane
+ * @author Matthis Collard
+ */
 public class LevelSelectHandler extends DynamicEventHandler{
+    
+    /**
+     * Static Integer of the current difficulty set on the grid (useful for creating the level selecting grid)
+     * @see LevelScene#initGrid
+     */
     public static int curDiff;
 
-    @FXML
-    private Button paramButton; 
-
-    @FXML
-    private Button powerButton;
-
+    /**
+     * Current background Panel
+     */
     @FXML
     private Pane backGround;
 
+    /**
+     * Difficulty button
+     */
     @FXML
     private Button diffButton;
 
+    /**
+     * Pane on which the selectiong grid will be added
+     */
     @FXML
     private Pane selectPane;
 
+    /**
+     * Initialization of LevelSelectHandler
+     * @param parent parent of the current Pane
+     */
     public LevelSelectHandler(Pane parent){
         super(parent);
         curDiff=1;
     }
 
-    public void paramClicked() throws Exception{
-        if (backGround.lookup("#pop") == null){
-            PopUp pop = new PopUp(backGround);
-            ParamHandler paramH= new ParamHandler(backGround);
-            pop.pasteAndHandle("/view/parameters.fxml", paramH);
-            paramH.setAll();
-            System.out.println("Je suis Paramètre");
-        }
-    }
-
-    public void quitClicked() {
-        Stage stage = (Stage) powerButton.getScene().getWindow();
-        stage.close();
-        System.out.println("Je quitte");
-    }
-
+    /**
+     * Action when diffButton is clicked. Changes the difficulty to chose from and reloads the level selection grid depending on the difficulty
+     * @throws Exception if the grid can't load
+     * @see LevelScene#initGrid
+     * @author Matthis Collard
+     */
     public void diffClicked() throws Exception{
         curDiff= curDiff%LevelScene.nbDiff +1;
         diffButton.setText(intToDiff(curDiff));
@@ -57,6 +61,12 @@ public class LevelSelectHandler extends DynamicEventHandler{
 
     }
 
+    /**
+     * Returns the difficulty as a string from a number
+     * @param diff difficulty (1,2 or 3)
+     * @return String representing difficulty (Example : 1 returns Facile)
+     * @author Matthis Collard
+     */
     public String intToDiff(int diff){
         switch (diff){
             case (1):
