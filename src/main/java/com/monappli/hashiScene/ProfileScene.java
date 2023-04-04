9207a375ab190dab86e5731384a9d81dd7989e00
Matthis1Collard <matthicollard@gmail.com>
2023-04-04 17:39:36 +0200

    début profile scene

---
+++ src/main/java/com/monappli/hashiScene/ProfileScene.java
@@ -0,0 +1,10 @@
+package com.monappli.hashiScene;
+
+import javafx.scene.layout.Pane;
+
+public class ProfileScene extends DynamicScene{
+    
+    public ProfileScene(Pane parent){
+        super(parent);
+    }
+}
