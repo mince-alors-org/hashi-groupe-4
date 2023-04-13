24546b78051efba7b89c052580acdc552053921b
GTerLeon <Leon.Ter.Etu@univ-lemans.fr>
2023-04-13 15:09:09 +0200

    fix grille.java

--- src/main/java/com/monappli/Grille.java
+++ src/main/java/com/monappli/Grille.java
@@ -237,13 +237,15 @@ public class Grille {
                      */
                     if (ilot.equals(pont.getIle1()) && ilot2.equals(pont.getIle2())) {
                       pont.affiche(fond);
+                      pontVoisin.setNombreTraits(pont.getNbTraits());
+                    }
                   }
                 }
               }
-            }
           }
       }
     }
+  
   
 
     public void ilotOnAction(Ilot ilot){
